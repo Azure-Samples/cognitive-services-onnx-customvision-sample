f8a03177edd8945364d7d5df8c2c1adba0ec2aa2
Kurt Kramer <kukramer@microsoft.com>
2018-05-01 17:10:49 -0700

    Initial Commit of Sample App

---
+++ MainPage.xaml.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.AI.MachineLearning.Preview;
+using Windows.Storage;
+using Windows.Media;
+using Windows.Graphics.Imaging;
+using System.Threading.Tasks;
+using Windows.Storage.Streams;
+using System.Diagnostics;
+using Windows.UI.Core;
+using Windows.Storage.Pickers;
+using Windows.UI.Xaml.Media.Imaging;
+
+/// <summary>
+/// How to add additional onnx model to this sample application.
+/// 1) Copy new onnx model to "Assets" subfolder.
+/// 2) Add model to "Project" under Assets folder by selecting "Add existing item"; navigate to new onnx model and add.
+///    Change properties "Build-Action" to "Content"  and  "Copy to Output Directory" to "Copy if Newer"
+/// 3) Add to list of supported models by adding entry to 'onnxFileNames' inializer below.
+///    When you add name of onnx model file name you also neeed to specify the number of lables the model contains.
+/// </summary>
+
+namespace SampleOnnxEvaluationApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private Stopwatch _stopwatch = new Stopwatch();
+        private OnnxModel _model = null;
+        private string _ourOnnxFileName = "CheesecakeDonutsFries.onnx";
+        private int _ourOnnxNumLables = 3;
+        private List<Tuple<string, int>> onnxFileNames = new List<Tuple<string,int>>()
+          {
+            new Tuple<string,int>("CheesecakeDonutsFries.onnx" , 3 ),
+            new Tuple<string,int>("Plankton.onnx", 4 )
+          };
+
+        public sealed class OnnxModelInput
+        {
+            public VideoFrame data { get; set; }
+        }
+
+        public sealed class OnnxModelOutput
+        {
+            public IList<string> classLabel { get; set; }
+            public IDictionary<string, float> loss { get; set; }
+            public OnnxModelOutput(int numLables)
+            {
+                this.classLabel = new List<string>();
+
+                // For dictionary(map) fields onnx needs the variable to be pre-allocatd such that the 
+                // length is equal to the number of labels defined in the model. The names are not
+                // required to match what is in the model.
+                this.loss = new Dictionary<string, float>();
+                for (int x = 0; x < numLables; ++x)
+                    this.loss.Add("Label_" + x.ToString(), 0.0f);
+            }
+        }
+
+        public sealed class OnnxModel
+        {
+            private LearningModelPreview learningModel = null;
+
+            public static async Task<OnnxModel> CreateOnnxModel(StorageFile file)
+            {
+                LearningModelPreview learningModel = null;
+
+                try
+                {
+                    learningModel = await LearningModelPreview.LoadModelFromStorageFileAsync(file);
+                }
+                catch (Exception e)
+                {
+                    var exceptionStr = e.ToString();
+                    System.Console.WriteLine(exceptionStr);
+                    throw e;
+                }
+                OnnxModel model = new OnnxModel();
+                learningModel.InferencingOptions.PreferredDeviceKind = LearningModelDeviceKindPreview.LearningDeviceGpu;
+                learningModel.InferencingOptions.ReclaimMemoryAfterEvaluation = true;
+                model.learningModel = learningModel;
+                return model;
+            }
+
+            public async Task<OnnxModelOutput> EvaluateAsync(OnnxModelInput input, int numLabels)
+            {
+                int zed = numLabels;
+                OnnxModelOutput output = new OnnxModelOutput(zed);
+                LearningModelBindingPreview binding = new LearningModelBindingPreview(learningModel);
+                binding.Bind("data", input.data);
+                binding.Bind("classLabel", output.classLabel);
+                binding.Bind("loss", output.loss);
+                LearningModelEvaluationResultPreview evalResult = await learningModel.EvaluateAsync(binding, string.Empty);
+                return output;
+            }
+        }
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            foreach (var x in onnxFileNames)
+            {
+                this.ActiveModel.Items.Add(x.Item1);
+            }
+            this.ActiveModel.SelectedIndex = 0;
+            _ourOnnxFileName = onnxFileNames[0].Item1;
+            _ourOnnxNumLables = onnxFileNames[0].Item2;
+        }
+
+        private async Task LoadModelAsync()
+        {
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => StatusBlock.Text = $"Loading {_ourOnnxFileName} ... patience ");
+
+            try
+            {
+                _stopwatch = Stopwatch.StartNew();
+
+                var modelFile = await StorageFile.GetFileFromApplicationUriAsync(new Uri($"ms-appx:///Assets/{_ourOnnxFileName}"));
+                _model = await OnnxModel.CreateOnnxModel(modelFile);
+
+                _stopwatch.Stop();
+                Debug.WriteLine($"Loaded {_ourOnnxFileName}: Elapsed time: {_stopwatch.ElapsedMilliseconds} ms");
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"error: {ex.Message}");
+                await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => StatusBlock.Text = $"error: {ex.Message}");
+                _model = null;
+            }
+        }
+
+        private async void ButtonRun_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonRun.IsEnabled = false;
+            UIPreviewImage.Source = null;
+            try
+            {
+                if (_model == null)
+                {
+                    // Load the model
+                    await Task.Run(async () => await LoadModelAsync());
+                }
+
+                // Trigger file picker to select an image file
+                FileOpenPicker fileOpenPicker = new FileOpenPicker();
+                fileOpenPicker.SuggestedStartLocation = PickerLocationId.PicturesLibrary;
+                fileOpenPicker.FileTypeFilter.Add(".bmp");
+                fileOpenPicker.FileTypeFilter.Add(".jpg");
+                fileOpenPicker.FileTypeFilter.Add(".png");
+                fileOpenPicker.ViewMode = PickerViewMode.Thumbnail;
+                StorageFile selectedStorageFile = await fileOpenPicker.PickSingleFileAsync();
+
+                SoftwareBitmap softwareBitmap;
+                using (IRandomAccessStream stream = await selectedStorageFile.OpenAsync(FileAccessMode.Read))
+                {
+                    // Create the decoder from the stream 
+                    BitmapDecoder decoder = await BitmapDecoder.CreateAsync(stream);
+
+                    // Get the SoftwareBitmap representation of the file in BGRA8 format
+                    softwareBitmap = await decoder.GetSoftwareBitmapAsync();
+                    softwareBitmap = SoftwareBitmap.Convert(softwareBitmap, BitmapPixelFormat.Bgra8, BitmapAlphaMode.Premultiplied);
+                }
+
+                // Display the image
+                SoftwareBitmapSource imageSource = new SoftwareBitmapSource();
+                await imageSource.SetBitmapAsync(softwareBitmap);
+                UIPreviewImage.Source = imageSource;
+
+                // Encapsulate the image in the WinML image type (VideoFrame) to be bound and evaluated
+                VideoFrame inputImage = VideoFrame.CreateWithSoftwareBitmap(softwareBitmap);
+
+                await Task.Run(async () =>
+                {
+                    // Evaluate the image
+                    await EvaluateVideoFrameAsync(inputImage);
+                });
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"error: {ex.Message}");
+                await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => StatusBlock.Text = $"error: {ex.Message}");
+            }
+            finally
+            {
+                ButtonRun.IsEnabled = true;
+            }
+        }
+
+        private async Task EvaluateVideoFrameAsync(VideoFrame frame)
+        {
+            if (frame != null)
+            {
+                try
+                {
+                    _stopwatch.Restart();
+                    OnnxModelInput inputData = new OnnxModelInput();
+                    inputData.data = frame;
+                    var results = await _model.EvaluateAsync(inputData, _ourOnnxNumLables);
+                    var loss = results.loss.ToList().OrderBy(x=>-(x.Value));
+                    var labels = results.classLabel;
+                    _stopwatch.Stop();
+
+                    var lossStr = string.Join(",  ", loss.Select(l => l.Key + " " + (l.Value * 100.0f).ToString("#0.00") + "%"));
+                    string message = $"Evaluation took {_stopwatch.ElapsedMilliseconds}ms to execute, Predictions: {lossStr}.";
+                    Debug.WriteLine(message);
+                    await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => StatusBlock.Text = message);
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine($"error: {ex.Message}");
+                    await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => StatusBlock.Text = $"error: {ex.Message}");
+                }
+
+                await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => ButtonRun.IsEnabled = true);
+            }
+        }
+
+        private void ActiveModel_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int selectedIdx = ActiveModel.SelectedIndex;
+            if  ((selectedIdx >= 0)  &&  (selectedIdx < onnxFileNames.Count))
+            {
+                _ourOnnxFileName = onnxFileNames[selectedIdx].Item1;
+                _ourOnnxNumLables = onnxFileNames[selectedIdx].Item2;
+                _model = null;  // Will force reloading of model.
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,57 +1,43 @@
-# Project Name
+---
+services: cognitive-services,custom-vision
+platforms: c#, Windows ML, XAML
+author: kukramer
+---
 
-(short, 1-3 sentenced, description of the project)
-
-## Features
-
-This project framework provides the following features:
-
-* Feature 1
-* Feature 2
-* ...
+# Sample application for ONNX models exported from Custom Vision Service
+This sample application demonstrates how to take a model exported from the [Custom Vision Service](https://www.customvision.ai) in the ONNX format and add it to an application for real-time image classification. 
 
 ## Getting Started
 
 ### Prerequisites
-
-(ideally very short, if any)
-
-- OS
-- Library version
-- ...
-
-### Installation
-
-(ideally very short)
-
-- npm install [package name]
-- mvn install
-- ...
-
+- [Windows SDK - Build 17110+] (latest)](https://www.microsoft.com/en-us/software-download/windowsinsiderpreviewSDK)
+- [Visual Studio 17](https://www.visualstudio.com/vs/preview/)
+- [Windows 10 Insider Preview](https://www.microsoft.com/en-us/software-download/windowsinsiderpreviewiso)
+- An account at [Custom Vision Service](https://www.customvision.ai) 
 ### Quickstart
-(Add steps to get up and running quickly)
 
-1. git clone [repository clone url]
-2. cd [respository name]
-3. ...
+1. clone the repository and open the project in Visual Studio
+2. Build and run the sample Application
+3. Application comes with two models already included along with sample images to test.
+### Adding your own sample model of your own classifier.
+The models provided with the sample recognizes some foods(Cheesecake, Donuts, Fries) and teh other recognizes some plankton images. To add  your own model exported from the [Custom Vision Service](https://www.customvision.ai) do the following, and then build and launch the application:
+  1. [Create and train](https://docs.microsoft.com/en-us/azure/cognitive-services/custom-vision-service/getting-started-build-a-classifier) a classifer with the Custom Vision Service. You must choose a "compact" domain such as **General (compact)** to be able to export your classifier. If you have an existing classifier you want to export instead, convert the domain in "settings" by clicking on the gear icon at the top right. In setting, choose a "compact" model, Save, and Train your project.  
+  2. Export your model by going to the Performance tab. Select an iteration trained with a compact domain, an "Export" button will appear. Click on *Export* then *TensorFlow* then *Export.* Click the *Download* button when it appears. A *.onnx file will download.
+  3. Drop your *model.onnx file into your project's Assets folder. 
+  4. Under Solutions Explorer/ Assets Folder add model file to project by selecting Add Existing Item.
+  5. Change properties of model just added: "Build Action" -> "Content"  and  "Copy to Output Directory" -> "Copy if newer"
+  6. Add to list variable "onnxFileNames" name of model just added along with number of lables model contains.
+  7. Build and run.
+  8. Use combo-box at to select Model you just added.
+  9. Click button to select image to evaluate.
 
-
-## Demo
-
-A demo app is included to show how to use the project.
-
-To run the demo, follow these steps:
-
-(Add steps to start up the demo)
-
-1.
-2.
-3.
+### Things to note.
+- Image preprocessing is performed by binding method call. Look at method "EvaluateAsync" of class "OnnxModel". Note that it binds a VideoFrame instance to "data"; this binding call will perform cropping and scaling such that the image fits the define size of model (227 x 227 with included models). It will also reformat the image data into a tensor that the model expects(Channel(BGR), Rows, Cols).
+- Dictionary(map) based output bindings must pre-allocate number of entries for number of labels model was trained for. Ex: The included Plankton model contains 4 classes(labels) and for this reason the output variable for “loss” has 4 entries added to it.  The name of the labels do not need to match the actual name of the classes in the model.
+- mlgen.exe - This tool generates API code in c# or c++ for a specified ONNX mode.  See [Windows ML overview](https://docs.microsoft.com/en-us/windows/uwp/machine-learning/overview) for description of utility. If you are working with a ONNX model and are unsure what variable types to utilize this utility will generate the correct types.
 
 ## Resources
-
-(Any additional resources or related projects)
-
-- Link to supporting information
-- Link to similar sample
-- ...
+- Link to [ONNX](https://onnx.ai/)
+- Link to [ONNX on GitHub](https://github.com/onnx/onnx)
+- Link to [Get started with Windows Machine Learning](https://docs.microsoft.com/en-us/windows/uwp/machine-learning/get-started)
+- Link to [Custom Vision Service Documentation](https://docs.microsoft.com/en-us/azure/cognitive-services/custom-vision-service/home)
